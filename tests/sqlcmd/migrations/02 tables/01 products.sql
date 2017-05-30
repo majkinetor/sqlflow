905d6fc2cb483b62e439e14ee98e2b5eb479b505
Miodrag Milic <miodrag.milic@gmail.com>
2017-05-31 01:31:10 +0200

    sdf

---
+++ tests/sqlcmd/migrations/02 tables/01 products.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Products](
+    [ID] [int] NOT NULL, 
+    [Name] [nvarchar](50) NOT NULL, 
+    [ListPrice] [money] NOT NULL
+ 
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([ID] ASC) 
+) 
+GO
--- tests/sqlcmd/migrations/02 tables/02 orders.sql
+++ tests/sqlcmd/migrations/02 tables/02 orders.sql
@@ -1,17 +1,9 @@
-CREATE TABLE [dbo].[Products](
-    [ID] [int] NOT NULL, 
-    [Name] [nvarchar](50) NOT NULL, 
-    [ListPrice] [money] NOT NULL
- 
-    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([ID] ASC) 
-) 
-GO
- 
 CREATE TABLE [dbo].[Orders]( 
     [OrderID] [int] NOT NULL, 
     [ProductID] [int] NOT NULL, 
     [Quantity] [int] NOT NULL, 
     [OriginState] [nvarchar](2) NOT NULL, 
+    
     CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED ([OrderID] ASC,[ProductID] ASC) 
 ) 
 GO
