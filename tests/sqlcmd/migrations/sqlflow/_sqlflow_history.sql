0c05e66316e418be6324b6981ab3b36e64591606
Miodrag Milic <miodrag.milic@gmail.com>
2017-05-31 12:22:32 +0200

    sc

--- tests/sqlcmd/migrations/sqlflow/_sqlflow_history.sql
+++ tests/sqlcmd/migrations/sqlflow/_sqlflow_history.sql
@@ -1,9 +1,10 @@
-CREATE TABLE "_sqlflow_history" (
-	RunId	   INT PRIMARY KEY,
-	StartDate  datetime2,
-	Duration   decimal,
-	Config     nvarchar(max),
-	Migrations nvarchar(max),
-	Changes    nvarchar(max),
-	Result	   nvarchar(max)
-);
+IF not exists (select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME='_sqlflow_history')
+	CREATE TABLE "_sqlflow_history" (
+		RunId	   INT PRIMARY KEY,
+		StartDate  datetime2,
+		Duration   decimal,
+		Config     nvarchar(max),
+		Migrations nvarchar(max),
+		Changes    nvarchar(max),
+		Result	   nvarchar(max)
+	);
---
+++ tests/sqlcmd/migrations/sqlflow/init_database.sql
@@ -0,0 +1,14 @@
+-- sqlflow: connection: admin
+ 
+IF NOT EXISTS(SELECT name FROM sys.databases WHERE name = 'test') 
+    CREATE DATABASE test
+
+-- CREATE DATABASE dbName
+-- ON (
+--   NAME = dbName_dat,
+--   FILENAME = 'D:\path\to\dbName.mdf'
+-- )
+-- LOG ON (
+--   NAME = dbName_log,
+--   FILENAME = 'D:\path\to\dbName.ldf'
+-- )
