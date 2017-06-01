b01d52fe623669d1a5fe4f17093b4d687c76506b
Miodrag Milic <miodrag.milic@gmail.com>
2017-06-02 00:13:30 +0200

    m

--- tests/sqlcmd/migrations/01 admin/01 create user.sql
+++ tests/sqlcmd/migrations/01 admin/01 create user.sql
@@ -1,7 +1,9 @@
 --|sqlflow| connection: admin
 
-Use test
-GO
+-- :setvar SQLCMDERRORLEVEL 1 -- Disable message for next USE command
+--     Use test
+--     GO
+-- :setvar SQLCMDERRORLEVEL 0
 
 IF NOT EXISTS (SELECT * FROM master.sys.server_principals WHERE name = 'test')
 BEGIN
@@ -11,7 +13,7 @@ BEGIN
         CHECK_EXPIRATION = OFF;
 END
 
-IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = N'test')
+IF NOT EXISTS (SELECT * FROM test.sys.database_principals WHERE name = N'test')
 BEGIN
     CREATE USER [test] FOR LOGIN [test]
     EXEC sp_addrolemember N'db_ddladmin',   N'test'
--- tests/sqlcmd/migrations/sqlflow/init_history.sql
+++ tests/sqlcmd/migrations/sqlflow/init_history.sql
@@ -1,11 +1,13 @@
 --|sqlflow| connection: admin
 
-USE test
-GO
+-- :setvar SQLCMDERRORLEVEL 1
+-- USE test
+-- GO
+-- :setvar SQLCMDERRORLEVEL 0
 
-IF not exists (select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME='_sqlflow_history')
+IF not exists (select * from test.INFORMATION_SCHEMA.TABLES where TABLE_NAME='_sqlflow_history')
 BEGIN
-	CREATE TABLE _sqlflow_history (
+	CREATE TABLE test.dbo._sqlflow_history (
 		RunId	   INT PRIMARY KEY,
 		StartDate  nvarchar(50),
 		Duration   decimal(18,2),
